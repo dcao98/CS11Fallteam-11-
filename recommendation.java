1753d3f686a31c9f5fa2c08a8bd0630f745ccc8e
dennisu742 <44779334+dennisu742@users.noreply.github.com>
2018-12-04 22:37:11 -0500

    Update recommendation.java

--- recommendation.java
+++ recommendation.java
@@ -3,38 +3,38 @@ public class Recommendation{
 
     String restaurant = TextIO.getlnString();
       if (a == korean){
-        System.out.println("Since you and " +celebrity+ "like Korean food,");
+        System.out.println("Since you and " +celebrity+ " both like Korean food,");
         System.out.println("We recommend you guys to go to:");
         System.out.println("1. Woori");
         System.out.println("2. Kaju Tofu House");
         System.out.println("3. Kimchipapi Kitchen");
 
       } else if (a == chinese){
-        System.out.println("Since you and " +celebrity+ "like Chinese food,");
+        System.out.println("Since you and " +celebrity+ " both like Chinese food,");
         System.out.println("We recommend you guys to go to: ");
         System.out.println("1. Asia Wok");
         System.out.println("2. Mulan Taiwanese Restaurant");
         System.out.println("3. Jin's Sichuan House");
       } else if (a == american){
-        System.out.println("Since you and " +celebrity+ "like American food,");
+        System.out.println("Since you and " +celebrity+ " both like American food,");
         System.out.println("We recommend you guys to go to: ");
         System.out.println("1. Bistro 781");
         System.out.println("2. The Gaff");
         System.out.println("3. Brewer's Tap & Table");
       } else if (a == italian){
-        System.out.println("Since you and " +celebrity+ "like Italian food,");
+        System.out.println("Since you and " +celebrity+ " both like Italian food,");
         System.out.println("We recommend you guys to go to: ");
         System.out.println("1. Fiorella's");
         System.out.println("2. Charcoal Guido's");
         System.out.println("3. Osteria Posto");
       } else if (a == fastfood){
-        System.out.println("Since you and " +celebrity+ "like Fast food,");
+        System.out.println("Since you and " +celebrity+ " both like Fast food,");
         System.out.println("We recommend you guys to go to: ");
         System.out.println("1. Five Guys");
         System.out.println("2. McDonalds");
         System.out.println("3. Wendy's");
       } else {
-        System.out.println("Since you and " +celebrity+ "like Japanese food,");
+        System.out.println("Since you and " +celebrity+ " both like Japanese food,");
         System.out.println("We recommend you guys to go to: ");
         System.out.println("1. Hakata Ramen & Sushi");
         System.out.println("2. UMI fine Japanese & Asian Cuisine");

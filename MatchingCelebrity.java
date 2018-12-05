fdb125cb5bd1c08dc8683996cb135e0616d5a2af
dennisu742 <44779334+dennisu742@users.noreply.github.com>
2018-12-04 21:43:34 -0500

    Update MatchingCelebrity.java

--- MatchingCelebrity.java
+++ MatchingCelebrity.java
@@ -1,22 +1,22 @@
 public class MatchingCelebrity{
     public static void main(String[]args){
         System.out.println("Hello! Are you excited to find your perfect celebrity match?");
-        System.out.println("We will only need a few information about you then you will find out! First, when is your birthday?");
+        System.out.println("We will only need a few information about you, then you will find out! First, when is your birthday?");
         String birthday = TextIO.getlnString();
         System.out.println("Ok, now a few more things... What is your favorite food?");
         String favoriteFood = TextIO.getlnString();
         System.out.println("What is your horoscope?");
         String horoscope = TextIO.getlnString();
-        System.out.println("What would you lke to do when you are alone... a. singing, b. dancing, c.acting... you have to pick one!");
+        System.out.println("What would you like to do when you are alone... a. singing, b. dancing, c.acting... you have to pick one!");
         String hobby = TextIO.getlnString();
         System.out.println("Do you perfer a male celebrity or a female celebrity?");
         String gender = TextIO.getlnString();
-        System.out.println("Ok, one last thing... you are so close to find your perfect match! What is your birth month?");
+        System.out.println("Ok, one last thing... you are so close to finding your perfect match! What is your month of birth?");
         String birthMonth= TextIO.getlnString();
 
         People myself = new people();
         String answer = Process.get(myself);
-        System.out.println(answer + " is your best match");
+        System.out.println(answer + " is your best match!");
 
     }
 }
